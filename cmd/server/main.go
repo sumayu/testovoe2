1508d2fcc112578d2d57a8b57c8beec668db0e81
Костя <konstantinsavin930@gmail.com>
2025-07-01 19:56:41 +0300

    рабочая версия не покрытая тестами и без докера

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,17 @@
 package main
 
-import "github.com/sumayu/testovoe2/internal/bd"
-func main()  {
-bd.Database()
+import (
+	"log"
+	"github.com/sumayu/testovoe2/internal/api"
+	"github.com/sumayu/testovoe2/internal/bd"
+)
+
+func main() {
+	db := bd.Database()
+	defer db.Close()
+	r := api.Router(db)
+	log.Println("Starting server on :8080")
+	if err := r.Run(":8080"); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
